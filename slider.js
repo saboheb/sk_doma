e43bb6e442c1149a10d4ba5cb8a6e101be2a2238
Alex Stolbov <100lbov21@gmail.com>
2024-03-12 22:29:05 +0400

    feat: add swipe

--- slider.js
+++ slider.js
@@ -4,14 +4,12 @@ let count = 0;
 let width;
 
 function init() {
-  console.log('resize');
   width = document.querySelector('.slider').offsetWidth;
   sliderLine.style.width = width * images.length + 'px';
   images.forEach(item => {
     item.style.width = width + 'px';
     item.style.height = 'auto';
   });
-  console.log(width);
 }
 
 window.addEventListener('resize', init);
@@ -38,3 +36,42 @@ document.querySelector('.arrow_next').addEventListener('click', function() {
 function rollSlider() {
   sliderLine.style.transform = 'translate(-' + count * width + 'px)';
 }
+
+//touch
+sliderLine.addEventListener('touchstart', handleTouchStart, false);
+sliderLine.addEventListener('touchmove', handleTouchMove, false);
+
+let posX1 = null;
+
+function handleTouchStart(event) {
+  const firstTouch = event.touches[0];
+  posX1 = firstTouch.clientX;
+  console.log(posX1);
+}
+
+function handleTouchMove(event) {
+  if (!posX1) {
+    return false;
+  }
+  let posX2 = event.touches[0].clientX;
+  let xDiff = posX2 - posX1;
+
+  if (xDiff < 0) {
+    console.log('right');
+    count++;
+    if (count >= images.length) {
+      count = images.length - 1;
+    }
+    rollSlider();
+
+  } else if (xDiff > 0) {
+    console.log('left');
+    count--;
+    if (count < 0) {
+    count = 0;
+  }
+  rollSlider();
+  }
+  posX1 = null;
+}
+
