c8a90a3e1038f7484067c0ed63943d6bd367f3e8
Alex Stolbov <100lbov21@gmail.com>
2024-03-22 22:03:53 +0400

    add php

---
+++ sendmail.php
@@ -0,0 +1,36 @@
+<?php
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+
+  require 'phpmailer/src/Exception.php';
+  require 'phpmailer/src/PHPMailer.php';
+
+  $mail = new PHPMailer(true);
+  $mail->CharSet = 'UTF-8';
+  $mail->setLanguage('ru', 'phpmailer/language/');
+  $mail->IsHTML(true);
+
+
+  $mail->setFrom('скдома18рф');// от кого 
+  $mail->addAddress('skdoma18@mail.ru'); // куда отправить
+  $mail->Subject = 'Новая заявка';
+
+  $body = 'Заявка с сайта';
+
+  $body.='<p><strong>Имя:</storng> '.$_POST['name'].'<p>';
+  $body.='<p><strong>Телефон:</strong> '.$_POST['phone'].'<p>';
+
+  $mail->Body = Sbody;
+
+  // отправка
+  if (!$mail->send()) {
+    $message = 'Ошибка';
+  } else {
+    $message = 'Данные отправлены!';
+  }
+
+  $response = ['message' => @message];
+
+  header('Content-type: application/json');
+  echo json_encode($response);
+?>
